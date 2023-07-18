a03f6dbe0ec178130652ec48f76f82dee0aeac86
Rebeca Borges <87546461+bexxborgs@users.noreply.github.com>
2023-07-18 12:56:42 -0300

    Update MinhaTabela.java

--- src/MinhaTabela.java
+++ src/MinhaTabela.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class MinhaTabela {
+@Tabela(nome = "minha_tabela")
+public class MinhaTabela {
+    public static void main(String[] args) {
+        Class<?> classe = MinhaTabela.class;
+
+        if (classe.isAnnotationPresent(Tabela.class)) {
+            Tabela tabela = classe.getAnnotation(Tabela.class);
+            String nomeTabela = tabela.nome();
+            System.out.println("Nome da tabela: " + nomeTabela);
+        }
+    }
 }
